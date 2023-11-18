a5f0550bbd31ef723f46596f5a7878d87180b898
NadaElBayoumy <nada.bayoumy1990@gmail.com>
2023-11-18 15:50:57 +0400

    nth

--- src/app/contact-map/contact-map.component.ts
+++ src/app/contact-map/contact-map.component.ts
@@ -23,7 +23,7 @@ export class ContactMapComponent implements OnInit {
 
 
     const customIcon = L.icon({
-      iconUrl: 'assets/pinpoint.jpg',
+      iconUrl: '../../assets/marker.svg',
       iconSize: [32, 32], // size of the icon
       iconAnchor: [16, 32], // point of the icon which will correspond to marker's location
       popupAnchor: [0, -32] // point from which the popup should open relative to the iconAnchor
